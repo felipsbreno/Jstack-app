5e1f10b4aceb978e8b16c1fd5693653a1da044f3
felipsbreno <brendo.filipe2050@gmail.com>
2022-11-14 20:51:51 -0300

    fazendo a conexão com o mongo

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
+mongoose
+  .connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
 
-app.listen(3333, () => {
-  console.log('🚀 Server is running in port 3333');
-});
+    app.listen(3333, () => {
+      console.log('🚀 Server is running in port 3333');
+    });
+  })
+  .catch((error) => console.log('Error the connect in database: ' + error));
