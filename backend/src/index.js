865f59ed78f871dbd11b65bc4d94eb20432e965e
felipsbreno <brendo.filipe2050@gmail.com>
2022-11-14 20:24:26 -0300

    initial app

---
+++ backend/src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var express_1 = require("express");
+var app = (0, express_1["default"])();
+app.listen(3333, function () {
+    console.log('🚀 Server is running!');
+});
