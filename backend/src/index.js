1640efa9f1509e50ac35e071e0e1d0e4ba63748c
felipsbreno <brendo.filipe2050@gmail.com>
2022-11-14 20:26:54 -0300

    remove index.js

--- backend/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var app = (0, express_1["default"])();
-app.listen(3333, function () {
-    console.log('🚀 Server is running!');
-});
